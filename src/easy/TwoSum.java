b5917520275defd07e406fd9eb9c4f96d6df533a
HwiNyeon Kim <z2b23173us@gmail.com>
2021-06-04 16:50:32 +0900

    [Easy] Two Sum : solved by One-pass Hash Table

--- src/easy/TwoSum.java
+++ src/easy/TwoSum.java
@@ -5,21 +5,17 @@ import java.util.Map;
 
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        // Trial #2 better solution
+        // Trial #3
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i); // value, index
-        }
-
-        for (int i = 0; i < nums.length; i++) {
             int leftover = target - nums[i];
-            if (map.containsKey(leftover) && map.get(leftover) != i) {
-                return new int[] {i, map.get(leftover)};
+            if (map.containsKey(leftover)) {
+                return new int[] {map.get(leftover), i};
             }
+            map.put(nums[i], i);
         }
-
         // should never reach here.
-        return new int[] {-1, -1};
+        return new int[] { -1, -1 };
     }
 }
 
